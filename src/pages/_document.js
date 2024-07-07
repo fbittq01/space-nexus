e8ac5780f8fe0460eaaab118652ea22cea398a95
Linea VietNam <35452526+LineaUnderscoreVietnam@users.noreply.github.com>
2024-07-08 03:45:46 +0700

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,15 +4,18 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta
-          property="og:title"
-          content="WEB3 NEXUS SPACE - the Space for Web3 Nexus!"
-        ></meta>
-        <meta
-          property="og:description"
-          content="WEB3 NEXUS SPACE is a pioneering Web3 project with the ambition to connect projects with every possible entity within the Web3 ecosystem..."
-        ></meta>
-        <meta property="og:image" content="https://ibb.co/Zh16YVZ"></meta>
+        <meta name="description" content="WEB3 NEXUS SPACE is a pioneering Web3 project with the ambition to connect projects with every possible entity within the Web3 ecosystem..."/>
+        <meta property="og:image" content="https://i.ibb.co/09QJV3t/preview.png"/>
+        <meta property="og:title" content="WEB3 NEXUS SPACE - the Space for Web3 Nexus!"/>
+        <meta property="og:description" content="WEB3 NEXUS SPACE is a pioneering Web3 project with the ambition to connect projects with every possible entity within the Web3 ecosystem..."/>
+        <meta name="twitter:description" content="WEB3 NEXUS SPACE is a pioneering Web3 project with the ambition to connect projects 
+        with every possible entity within the Web3 ecosystem..."/>
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:title" content="WEB3 NEXUS SPACE - the Space for Web3 Nexus!"/>
+        <meta name="twitter:image" content="https://i.ibb.co/09QJV3t/preview.png"/>
+        <meta property="og:type" content="page"/>
+        <meta name="keywords" content="web3, connect, nexus"/>
+      </Head>
       </Head>
       <body>
         <Main />
